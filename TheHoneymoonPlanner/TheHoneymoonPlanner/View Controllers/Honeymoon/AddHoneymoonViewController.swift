ea4180b6e2874cfb3f839c961f282fd042583cf7
jonalynn masters <jonalynna.masters@gmail.com>
2020-02-05 12:58:47 -0800

    updated datePicker in +HMVC

--- TheHoneymoonPlanner/TheHoneymoonPlanner/View Controllers/Honeymoon/AddHoneymoonViewController.swift
+++ TheHoneymoonPlanner/TheHoneymoonPlanner/View Controllers/Honeymoon/AddHoneymoonViewController.swift
@@ -17,16 +17,63 @@ class AddHoneymoonViewController: UIViewController {
 
     @IBOutlet weak var honeymoonNameTextField: UITextField!
     @IBOutlet weak var selectLocationButton: UIButton!
+    @IBOutlet weak var startTextField: UITextField!
     
+    @IBOutlet weak var endTextField: UITextField!
+    
+    private var startDatePicker: UIDatePicker?
+    private var endDatePicker: UIDatePicker?
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    super.viewDidLoad()
+    
+        startDatePicker = UIDatePicker()
+        endDatePicker = UIDatePicker()
+        startDatePicker?.datePickerMode = .dateAndTime
+        endDatePicker?.datePickerMode = .dateAndTime
+        
+        startDatePicker?.addTarget(self, action: #selector(AddHoneymoonViewController.startDateChanged(datePicker:)), for: .valueChanged)
+         endDatePicker?.addTarget(self, action: #selector(AddHoneymoonViewController.endDateChanged(datePicker:)), for: .valueChanged)
+        
+        let startTapGesture = UITapGestureRecognizer(target: self, action: #selector(AddHoneymoonViewController.startViewTapped(gestureRecognizer:)))
+        
+        let endTapGesture = UITapGestureRecognizer(target: self, action: #selector(AddHoneymoonViewController.endViewTapped(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(startTapGesture)
+        startTextField.inputView = startDatePicker
+        
+        view.addGestureRecognizer(endTapGesture)
+        endTextField.inputView = endDatePicker
     }
     
     @IBAction func cancelTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func startViewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @objc func startDateChanged(datePicker: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        
+        startTextField.text = dateFormatter.string(from: datePicker.date)
+    }
+    
+    @objc func endViewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @objc func endDateChanged(datePicker: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        
+        endTextField.text = dateFormatter.string(from: datePicker.date)
     }
     
     /*
